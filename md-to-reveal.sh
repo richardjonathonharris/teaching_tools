fa9e6ca47b8a6d5aa358faa13168462d140db521
Richard Harris <richard.jonathon.harris@gmail.com>
2016-09-12 16:35:33 -0500

    Added second arg for md-to-reveal

--- md-to-reveal.sh
+++ md-to-reveal.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
 
-`pandoc -t revealjs -s $1 -o ~/reveal.js/decks/ga/current.html`
-`grep -r -l 'reveal.js' ~/reveal.js/decks/ga | xargs sed 's#reveal.js/#../../#g' > ~/reveal.js/decks/ga/ready.html`
-echo "localhost:8000/decks/ga/ready.html"
+`pandoc -t revealjs -s $1 -o ~/reveal.js/decks/ga/$2.html`
+`grep -r -l 'reveal.js' ~/reveal.js/decks/ga | xargs sed 's#reveal.js/#../../#g' > ~/reveal.js/decks/ga/$2.html`
+echo "http://localhost:8000/decks/ga/ready.html"
